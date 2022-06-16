8e26ac2ebc3f6310e9efeba0f4fa73ac72b74c75
aidanjungo <aidan@cfse.ch>
2022-06-16 14:21:49 +0200

    Improve layout

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 import streamlit as st
 from directorypicker import st_directory_picker
 
-my_selected_path = st_directory_picker()
+
+st_directory_picker()
+
+code1 = """import streamlit as st
+from directorypicker import st_directory_picker
+
+st_directory_picker()
+st.markdown(st.session_state.path)"""
+
+code2 = """my_selected_path = st_directory_picker()
+st.markdown(my_selected_path)"""
 
 st.markdown("---")
-st.markdown(
-    "You can access the selected path with the session state: `st.session_state.path`"
-)
-st.markdown(my_selected_path)
+st.markdown("You can access the selected path from: ")
+st.markdown("- The session state `st.session_state.path`:")
+st.code(code1, language="python")
+st.markdown("- return value of the function:")
+st.code(code2, language="python")
--- directorypicker.py
+++ directorypicker.py
@@ -9,42 +9,40 @@ def st_directory_picker(initial_path=Path()):
     if "path" not in st.session_state:
         st.session_state.path = initial_path.absolute()
 
-    col1, col2, col3, col4 = st.columns([7, 1, 3, 1])
+    st.text_input("Selected directory:", st.session_state.path)
+
+    _, col1, col2, col3, _ = st.columns([3, 1, 3, 1, 3])
 
     with col1:
-        st.markdown("#")
-        st.markdown("..." + str(st.session_state.path)[-42:])
-
-    with col2:
         st.markdown("#")
         if st.button("⬅️") and "path" in st.session_state:
             st.session_state.path = st.session_state.path.parent
             st.experimental_rerun()
 
-    with col3:
-        sub_directroies = [
+    with col2:
+        subdirectroies = [
             f.stem
             for f in st.session_state.path.iterdir()
             if f.is_dir()
             and (not f.stem.startswith(".") and not f.stem.startswith("__"))
         ]
-        if sub_directroies:
-            st.session_state.new_dir = st.selectbox("", sorted(sub_directroies))
+        if subdirectroies:
+            st.session_state.new_dir = st.selectbox(
+                "Subdirectories", sorted(subdirectroies)
+            )
         else:
             st.markdown("#")
             st.markdown(
                 "<font color='#FF0000'>No subdir</font>", unsafe_allow_html=True
             )
 
-    with col4:
-        if sub_directroies:
+    with col3:
+        if subdirectroies:
             st.markdown("#")
             if st.button("➡️") and "path" in st.session_state:
                 st.session_state.path = Path(
                     st.session_state.path, st.session_state.new_dir
                 )
                 st.experimental_rerun()
-
-    st.markdown(f"**Selected directory:** {st.session_state.path}")
 
     return st.session_state.path
